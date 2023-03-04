21687a864e398e92a2512000263c7412a2123262
Gusfavoreto <gus@favoreto.co>
2023-03-03 22:30:00 -0600

    e

---
+++ Week10/Week11_square/Week11_square/Week11_square.cpp
@@ -0,0 +1,28 @@
+
+
+#include <iostream>
+using namespace std;
+
+class Circle {
+public:
+   int radius;
+   double Area() {
+       return 3.141 * radius * radius;
+   }
+};
+
+class Rectangle {
+public:
+    int height;
+    int width;
+    int area() {
+        return height * width;
+    }
+
+};
+
+
+int main()
+{
+    
+}
